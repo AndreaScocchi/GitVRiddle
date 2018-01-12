c28c30205cb4e398e16b937171478b787fe35963
Tommaso <tommasoceschia@gmail.com>
2018-01-12 16:22:18 +0100

    Furiog si muove

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 1;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position += new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z) * Time.deltaTime * moveSpeed;
+        }
+    }
+}
