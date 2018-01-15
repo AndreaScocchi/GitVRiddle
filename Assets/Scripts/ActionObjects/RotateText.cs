be745db685a2842d8a789213673e1f2dfe169d20
Tommaso <tommasoceschia@gmail.com>
2018-01-15 17:18:42 +0100

    Furiog si muove e interagisce

---
+++ Assets/Scripts/ActionObjects/RotateText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateText : MonoBehaviour {
+
+    Vector3 direction;
+
+    void Update()
+    {
+        direction = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerRotation: MonoBehaviour {
+
+    public bool RotateX = false;
+    private float XRotation = 0f;
+
+    void Update () {
+        if (RotateX) {
+            XRotation = Camera.main.transform.eulerAngles.x;
+            if (XRotation > 50 && XRotation < 90) {
+                XRotation = 50f;
+            }
+            transform.rotation = Quaternion.Euler(XRotation, Camera.main.transform.eulerAngles.y, 0);
+        } else {
+            transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+        }
+    }
+}
